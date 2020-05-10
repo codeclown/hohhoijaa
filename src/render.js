8bcab378c331ec7916586908ac2c07310c534944
Martti Laine <martti@codeclown.net>
2020-05-10 14:35:30 +0200

    Render indexes to duplicate headings

--- src/render.js
+++ src/render.js
@@ -1,8 +1,11 @@
 const render = headings => {
+  const indexes = {};
   return headings.map(({ level, text }) => {
     const indent = '  '.repeat(level - 1);
     const slug = text.toLowerCase().trim().replace(/\s+/g, '-').replace(/[^a-z-]/g, '');
-    return `${indent}- [${text}](#${slug})`;
+    const index = indexes[slug] !== undefined ? indexes[slug] + 1 : 0;
+    indexes[slug] = index;
+    return `${indent}- [${text}](#${slug}${index === 0 ? '' : `-${index}`})`;
   }).join('\n');
 };
 
--- src/render.test.js
+++ src/render.test.js
@@ -12,23 +12,37 @@ describe('render', () => {
       { level: 2, text: 'Heading' },
       { level: 3, text: 'Sub-heading' },
       { level: 4, text: 'Sub-sub-heading' },
-      { level: 2, text: 'Heading' },
+      { level: 2, text: 'Asd' },
       { level: 1, text: 'Major heading' },
-      { level: 2, text: 'Sub-sub-heading' },
+      { level: 2, text: 'Foobar' },
       { level: 2, text: '`code`' },
-      { level: 2, text: 'Sub-heading' },
-      { level: 3, text: 'Sub-sub-heading' }
+      { level: 2, text: 'Hey there' },
+      { level: 3, text: 'Yay' }
     ]), `
 - [Heading levels](#heading-levels)
   - [Heading](#heading)
     - [Sub-heading](#sub-heading)
       - [Sub-sub-heading](#sub-sub-heading)
-  - [Heading](#heading)
+  - [Asd](#asd)
 - [Major heading](#major-heading)
-  - [Sub-sub-heading](#sub-sub-heading)
+  - [Foobar](#foobar)
   - [\`code\`](#code)
-  - [Sub-heading](#sub-heading)
-    - [Sub-sub-heading](#sub-sub-heading)
+  - [Hey there](#hey-there)
+    - [Yay](#yay)
+    `.trim());
+  });
+
+  it('renders duplicate headings with index', () => {
+    assert.deepStrictEqual(render([
+      { level: 1, text: 'Heading' },
+      { level: 2, text: 'Heading' },
+      { level: 3, text: 'Heading' },
+      { level: 2, text: 'Heading' },
+    ]), `
+- [Heading](#heading)
+  - [Heading](#heading-1)
+    - [Heading](#heading-2)
+  - [Heading](#heading-3)
     `.trim());
   });
 });
